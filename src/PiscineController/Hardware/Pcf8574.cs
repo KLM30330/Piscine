44b8f2e97bd521a27ca52f424c6e4db09025871e
Gabriel BERGIER <gabriel.bergier@exakis-nelite.com>
2026-06-13 12:25:57 +0200

    feat: Pcf8574 relay expander + Lcd1602 I2C display

---
+++ src/PiscineController/Hardware/Pcf8574.cs
@@ -0,0 +1,29 @@
+using System.Device.I2c;
+
+namespace PiscineController.Hardware;
+
+public sealed class Pcf8574 : IDisposable
+{
+    private readonly I2cDevice _device;
+    private byte _state;
+
+    public Pcf8574(int busId, int address)
+    {
+        _device = I2cDevice.Create(new I2cConnectionSettings(busId, address));
+        _state = 0x00;
+        Write();
+    }
+
+    public void SetPin(int pin, bool value)
+    {
+        if (value) _state |=  (byte)(1 << pin);
+        else       _state &= (byte)~(1 << pin);
+        Write();
+    }
+
+    public bool GetPin(int pin) => (_state & (1 << pin)) != 0;
+
+    private void Write() => _device.WriteByte(_state);
+
+    public void Dispose() => _device.Dispose();
+}
